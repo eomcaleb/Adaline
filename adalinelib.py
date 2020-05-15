7b26334caee7d9ec6674aa3f3ae90e893e0b022e
unknown <caleb.eom@getfreepoint.com>
2020-05-14 21:09:34 -0400

    minor changes

--- adalinelib.py
+++ adalinelib.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Adaline(object):
-	def __init__(self, ninput, epochs = 40, learning_rate=0.1, random_weights = True):
+	def __init__(self, ninput, epochs = 40, learning_rate=0.01, random_weights = True):
 		self.epochs = epochs
 		self.learning_rate = learning_rate
 		self.ninput = ninput
@@ -11,45 +11,27 @@ class Adaline(object):
 			self.weights = np.random.rand(ninput + 1)
 
 	# predict - used for training the model to get the output/sum
-	def predict(self, inputs):
+	def linearfunction(self, inputs):
 		summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
-		return summation
-	
+		return summation	
+
 	# reallifepredict - used after training with real life data set
-	def reallifepredict(self, inputs):
+	def predict(self, inputs):
 		if (np.dot(inputs, self.weights[1:]) + self.weights[0]) > 0.0:
 			return 1
 		return -1
-
-	# costfunction - prints cost function through each epoch
-	def costfunction(self):
-		print("\nCost Function")
-		print("Number of epoch from last iteration = ", len(self.cost))
-		print(self.cost)
 
 	# train - explicit programming the training procedure
 	def train(self, trainingdata, targets, verbose = False):
 		self.cost = []
 		for epoch in range(self.epochs): 
-			error = []
 			for inputs, target in zip(trainingdata, targets):
-				predict = self.predict(inputs)
+				predict = self.linearfunction(inputs)
+				# Stochastic Gradient Descent
 				self.weights[1:] += self.learning_rate * (target - predict) * inputs
 				self.weights[0] += self.learning_rate * (target - predict)
-				error.append((target - predict) ** 2)
-			self.cost.append(np.sum(error) / self.ninput)
-			if (verbose):
-				print ("Epoch #", epoch, "; Bias: ", self.weights[0], "; Weights:", self.weights[1:])	
+				# Keep track of cost 
+				self.cost.append(np.sum((target - predict) ** 2)/ self.ninput)
+			if (verbose and epoch % 10 == 0):
+				print ("Epoch #", epoch, "; Bias: ", self.weights[0], "; Weights:", self.weights[1:], "; Error = ", (np.sum((target - predict) ** 2)/ self.ninput))	
 	
-	# train2 - implicit numpy library call for training procedure (same result as train) 
-	def train2(self, trainingdata, targets, verbose = False):
-		self.cost = []
-		trainingdata = np.copy(trainingdata)
-		for epoch in range(self.epochs): 
-			output = self.predict(trainingdata)
-			errors = (targets - output)
-			self.weights[1:] += self.learning_rate * trainingdata.T.dot(errors)
-			self.weights[0] += self.learning_rate * errors.sum()
-			self.cost.append((errors ** 2).sum() / self.ninput)
-			if (verbose):
-				print ("Epoch #", epoch, "; Bias: ", self.weights[0], "; Weights:", self.weights[1:])	
--- main.py
+++ main.py
@@ -2,24 +2,21 @@ import numpy as np
 from adalinelib import Adaline
 
 # Training data set
-# NOTE: bi-polar input is used for ADALINE
 training_inputs = []
 training_inputs.append(np.array([1,1]))
 training_inputs.append(np.array([1,-1]))
 training_inputs.append(np.array([-1,1]))
 training_inputs.append(np.array([-1,-1]))
 
-# AND GATE
-labels = np.array([1,-1,-1,-1]) 
+# OR GATE
+labels = np.array([1,1,1,-1]) 
 
 # ADALINE
 adaline = Adaline(2)
 adaline.train(training_inputs, labels, True)
-#adaline.train2(training_inputs, labels, True)
 
 # Real Data Set
 print ("Real Data Set")
 input_1 = np.array([1,1])
 print ("Input: ", input_1)
-print (adaline.reallifepredict(input_1))
-adaline.costfunction()
+print (adaline.predict(input_1))
